3c97f889f0f21764da284ba9a7644143e377348c
Dorian L.O <dalo1997@hotmail.es>
2023-06-01 12:06:33 -0500

    Fix Prompts ChatGPT - Limite de Caracteres a 3000 para todos los informes

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1729,7 +1729,7 @@ dashboardController.enviarCuestionario = async (req, res) => {
                 'Metas a corto plazo': { m1, m2, m3, m4, m5 }
             }
 
-            const prompt = (JSON.stringify(obj_respuestas)+" Con base en las respuestas anteriores genera un informe de diagnóstico separado por las 4 dimensiones: Producto, Administración, Operaciones, Marketing. Que incluya las oportunidades de mejora, sugerencias, actividades a realizar.")
+            const prompt = (JSON.stringify(obj_respuestas)+" Con base en las respuestas anteriores genera un informe de diagnóstico separado por las 4 dimensiones: Producto, Administración, Operaciones, Marketing. Que incluya las oportunidades de mejora y sugerencias en cada dimensión. El informe no debe superar los 3000 caracteres.")
             console.log(`\n\n\n *:*:*:*:*:*:*:*:*:*:*:*:* \n\n PROMPT ENVIADO AL CHAT GPT *:*:*:*:*:*:*:*:*:* \n\n ${prompt} \n\n`);
             let resultAI = await getResponseChatGPT(prompt)
             const resp = resultAI.content.replaceAll('\n', '<br>');
@@ -2006,7 +2006,7 @@ dashboardController.guardarRespuestas = async (req, res) => {
                 'Metas': metas
             }
 
-            const prompt = (JSON.stringify(obj_respuestas)+" Con base en las respuestas anteriores genera un informe de diagnóstico separado por dimensiones que incluya las oportunidades de mejora, sugerencias, actividades a realizar.")
+            const prompt = (JSON.stringify(obj_respuestas)+" Con base en las respuestas anteriores genera un informe de diagnóstico separado por dimensiones que incluya las oportunidades de mejora, sugerencias, actividades a realizar. El informe no debe superar los 3000 caracteres.")
             console.log(`\n\n\n *:*:*:*:*:*:*:*:*:*:*:*:* \n\n PROMPT ENVIADO AL CHAT GPT *:*:*:*:*:*:*:*:*:* \n\n ${prompt} \n\n`);
             let resultAI = await getResponseChatGPT(prompt)
             const resp = resultAI.content.replaceAll('\n', '<br>');
