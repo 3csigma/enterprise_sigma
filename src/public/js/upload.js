af2c6aee74bded8296cdeb114521d59e2292415d
jcarlosabc <jeancatovio23@gmail.com>
2023-06-18 14:43:28 -0500

    Fix arreglar fusion - iconos repetidos en grupos - información cruzada entre los grupos - archivos sueltos - ida de objeto vacio a la tabla (recurso_armado) - FALTA (campos dinamicos cuando los grupos ya fueron creados)

--- src/public/js/upload.js
+++ src/public/js/upload.js
@@ -1,78 +1,145 @@
-const dropArea = document.querySelector(".drop-area");
+let fileId = null;
+
+const dropArea = document.querySelector(".drag-area");
 const dragText = dropArea.querySelector("h2");
 const button = dropArea.querySelector("#btnfile");
-const input = dropArea.querySelector("#inputt-file");
-let files;
+const input = dropArea.querySelector("#input-file");
 
 button.addEventListener("click", (e) => {
-    input.click();
+  e.preventDefault();
+  input.click();
 });
 
-input.addEventListener("change", (e) => {
-    files = e.target.files;
-    dropArea.classList.add("active");
-    showFiles(files);
-    dropArea.classList.remove("active");
+input.addEventListener("change", () => {
+  const file = input.files[0];
+  showFile(file);
 });
 
-document.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    dropArea.classList.add("active");
-    dragText.textContent = "Suelta para subir los archivos";
+dropArea.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropArea.classList.add("active");
+  dragText.textContent = "Suelta para subir el archivo";
 });
 
-document.addEventListener("dragleave", (e) => {
-    e.preventDefault();
-    dropArea.classList.remove("active");
-    dragText.textContent = "Arrastra y suelta archivos";
+dropArea.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  dropArea.classList.remove("active");
+  dragText.textContent = "Arrastra y suelta el archivo";
 });
 
-document.addEventListener("drop", (e) => {
-    e.preventDefault();
-    dropArea.classList.remove("active");
-    dragText.textContent = "Arrastra y suelta archivos";
-    files = e.dataTransfer.files;
-    showFiles(files);
+dropArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+  const file = e.dataTransfer.files[0];
+  showFile(file);
+  dropArea.classList.remove("active");
+  dragText.textContent = "Arrastra y suelta el archivo";
 });
 
-function showFiles(files) {
-    if (files.length === undefined) {
-        processFile(files);
-    } else {
-        for (const file of files) {
-            processFile(file);
-        }
+function showFile(file) {
+  const docType = file.type;
+  const extension = file.name.split('.').pop().toLowerCase();
+
+  const validExtensions = {
+    pdf: 'application/pdf',
+    word: ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
+    powerpoint: ['application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.presentationml.slideshow', 'application/vnd.openxmlformats-officedocument.presentationml.slide', 'application/pptx'],
+    excel: ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
+    image: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
+  };
+
+  let fileType = '';
+  let logoUrl = '';
+
+  for (const key in validExtensions) {
+    if (validExtensions[key].includes(docType) || validExtensions[key].includes(extension)) {
+      fileType = key;
+      break;
     }
-}
+  }
 
-function processFile(file) {
-   const docType = file.type;
-   const validarExtension = ['image/jpeg','image/jpg', 'image/png', 'image/gif' ]
-   const fileReader = new fileReader()
-   const id = `file-${Math.random().toString(32).substring(7)}`;
+  switch (fileType) {
+    case 'pdf':
+      logoUrl = '../logos_recursos/Documento_PDF.svg';
+      break;
+    case 'word':
+      logoUrl = '../logos_recursos/Documento_Word.svg';
+      break;
+    case 'powerpoint':
+      logoUrl = '../logos_recursos/Documento_PowerPoint.svg';
+      break;
+    case 'excel':
+      logoUrl = '../logos_recursos/Documento_Excel.svg';
+      break;
+    case 'image':
+      logoUrl = '../logos_recursos/Archivo_imagen.svg';
+      break;
+    default:
+      Swal.fire({
+        title: 'Archivo no permnitido',
+        text: "",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#50368C',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'ok'
+    })
+      return;
+  }
 
-   fileReader.addEventListener("load", (e) => {
-    e.preventDefault();
-    const fileUrl = fileReader.result
-    const image = `
-        <div id="${id}" class="file-container">
-            <img src="${fileUrl}">
-            <div class="status">
-                <span>${file.name}</span>
-                <span class="status-text"Cargando...</span>
-            </div>
+  const fileReader = new FileReader();
+
+  fileReader.addEventListener("load", () => {
+
+    if (fileId) {
+      const existingFile = document.getElementById(fileId);
+      existingFile.querySelector('.file-logo').src = logoUrl;
+      existingFile.querySelector('.status span').textContent = file.name;
+    } else {
+      fileId = `file-${Math.random().toString(32).substring(7)}`;
+
+      const image = `
+        <div id="${fileId}" class="file-container">
+          <img src="${logoUrl}" class="file-logo" width="35px">
+          <div class="status">
+            <span>${file.name}</span>
+            <span class="status-text">Cargando...</span>
+          </div>
         </div>
-    `;
-    const html = document.querySelector('#preview').innerHTML
-    document.querySelector('#preview').innerHTML = image + html;
-   })
-   fileReader.readAsdDataURL(file);
-   uploadFile(file, id)
-   if (validarExtension.includes(docType)) {
-    console.log("Si papu");
-   }else {
-    console.log("NO papu");
-   }
+      `;
+
+      const preview = document.querySelector('#preview');
+      preview.innerHTML = image;
+    }
+
+    uploadFile(file);
+  });
+
+  fileReader.readAsDataURL(file);
 }
 
-function uploadFile(file){}
+function uploadFile(file) {
+  const formData = new FormData();
+  formData.append('file', file);
+  console.log("Uploading file", file);
+  fetch('/enviar-archivo', {
+    method: 'POST',
+    body: formData,
+    headers: { 'enctype': 'multipart/form-data' }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const statusText = document.querySelector(`#${fileId} .status-text`);
+      if (response.ok) {
+        statusText.innerHTML = `<span class="success">Archivo subido correctamente</span>`;
+      } else {
+        statusText.innerHTML = `<span class="failure">Archivo no pudo subirse...</span>`;
+      }
+      // Mostrar la respuesta del servidor en el DOM
+      const responseText = document.createElement('p');
+      responseText.textContent = data.message;
+      statusText.appendChild(responseText);
+    })
+    .catch(error => {
+      document.querySelector(`#${fileId} .status-text`).innerHTML = `<span class="success">Archivo leido...</span>`;
+    });
+}
